0b819bc0338265068a053b8648d9b18f52d78de6
diogofernandesc <diogofernandescon@gmail.com>
2017-12-03 15:40:47 +0000

    tests issue fix 10

--- db_engine_tests.py
+++ db_engine_tests.py
@@ -5,14 +5,17 @@ from cons import DB
 
 class DBFindTest(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     def find_document(self):
         db_connection = DBConnection(deploy=True)
         result = db_connection.find_document(collection=DB.MP_COLLECTION,
                                              filter={"twitter_handle": "@theresa_may"},
                                              projection={"name": 1, "_id": 0})[0]
 
-        self.assertEqual(result["name"], "Theresa May")
         db_connection.close()
+        self.assertEqual(result["name"], "Theresa May")
 
 
 if __name__ == '__main__':
