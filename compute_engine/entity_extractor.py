f0bee09a909f33a5db11f3aeef2ddbba7f663959
diogofernandesc <diogofernandescon@gmail.com>
2018-04-09 12:31:25 +0100

    Added new API for entity extraction + types

--- compute_engine/entity_extractor.py
+++ compute_engine/entity_extractor.py
@@ -8,13 +8,18 @@ from tweet_handler import TweetHandler
 from nltk.corpus import stopwords
 import nltk
 import time
+import logging
+from rosette.api import API, DocumentParameters, RosetteException
 
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
 
 class EntityExtractor(TweetHandler):
     def __init__(self):
         super(EntityExtractor, self).__init__()
         self.nlu = NaturalLanguageUnderstandingV1(version='2017-02-27',
                                                   username=os.getenv('IBM_USER'), password=os.getenv('IBM_PASS'))
+        self.rosette = API(user_key=os.getenv("ROSETTE_API_KEY"))
 
     def get_clean(self, filter={}, limit=1000, tweet=None):
         """
@@ -38,7 +43,6 @@ class EntityExtractor(TweetHandler):
             tweet_text = re.sub(regex_remove, '', tweet["text"]).strip()
             tweet_id = tweet["_id"]
 
-
             stopword_list = []
             stopword_file = open('stopwords.txt', 'r')
             for line in stopword_file:
@@ -51,6 +55,25 @@ class EntityExtractor(TweetHandler):
 
         return clean_tweets
 
+    def analyse_rosette(self, tweet):
+        params = DocumentParameters()
+        params["content"] = tweet[1]
+        params["genre"] = "social-media"
+
+        extracted_entities = []
+        try:
+            entities = self.rosette.entities(params)
+            for entity in entities['entities']:
+                extracted_entities.append({
+                    "entity": entity['mention'],
+                    "type": entity["type"]
+
+                })
+            return extracted_entities
+
+        except RosetteException as exception:
+            logger.warn("Rossete API exception: %s" % exception)
+
     def analyse(self, tweets):
         """
         Extract keywords and entities from tweets
@@ -62,18 +85,27 @@ class EntityExtractor(TweetHandler):
             keywords = []
             entities = []
             response = self.nlu.analyze(text=tweet[1], features=Features(keywords=KeywordsOptions(),
-                                                                     entities=EntitiesOptions(),
-                                                                     relations=RelationsOptions()))
+                                                                         entities=EntitiesOptions()))
+            rosette_entities = self.analyse_rosette(tweet=tweet)
+            entities = entities + rosette_entities
+
             for keyword in response['keywords']:
                 keywords.append(keyword['text'])
 
             for entity in response['entities']:
-                entities.append(entity['text'])
+                entities.append({
+                    "entity": entity['text'],
+                    "type": entity["type"]
+                })
 
+
+            print tweet[0]
             result_tweet = self.db_connection.find_and_update(collection=DB.TWEET_COLLECTION,
                                                               query={"_id": tweet[0]},
                                                               update={"$set": {"keywords": keywords,
                                                                                "entities": entities}})
+
+            print result_tweet
 
 
 if __name__ == "__main__":
@@ -82,6 +114,10 @@ if __name__ == "__main__":
     while True:
         # Get tweets that have not been analysed yet
         tweets = ext.get_clean(filter={"$or": [{"keywords": None}, {"entities": None}]})
+        # tweets = ext.db_connection.find_document(collection=DB.TWEET_COLLECTION,
+        #                                           filter={"$or": [{"keywords": None}, {"entities": None}]},
+        #                                           projection={"text": 1},
+        #                                           limit=10)
         if tweets:
             ext.analyse(tweets=tweets)
 
